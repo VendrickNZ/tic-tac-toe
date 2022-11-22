8d80d18355d4889d459f92bb79744c15109095a4
VendrickNZ <jakibisherwood88@gmail.com>
2022-11-23 10:47:41 +1300

    made board gui

---
+++ script.js
@@ -0,0 +1,39 @@
+const gameBoard = (() => {
+
+    let board = Array(9)
+
+    //const makeMove = (move) => boar
+
+    //return all of them
+    return {
+
+    }
+})();
+
+const displayController = (() => {
+    //functions
+    const gameBoardUI = document.querySelector('.game-board');
+    for (let i = 0; i < 9; i++) {
+        let gameGrid = document.createElement('button');
+        gameGrid.classList = "game-grid-object"
+        gameBoardUI.appendChild(gameGrid);
+
+    }
+
+
+    //return all of them
+    return {
+
+    }
+})();
+
+const Player = (() => {
+    //functions
+    
+
+
+    //return all of them
+    return {
+
+    }
+});

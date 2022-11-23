e9eacd66f9c2c5c257355c51ed45d0b2875fb63a
VendrickNZ <jakibisherwood88@gmail.com>
2022-11-23 13:18:16 +1300

    added restart

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+function changeBoard(board) {
+
+}
+
+function setMove(playersMarker, board, noughts, crosses, gameGrid) {
+    if (playersMarker == "X") {
+        noughts.style.visibility = 'hidden';
+    } else {
+        crosses.style.visibility = 'hidden';
+    }
+    gameGrid.innerText = playersMarker;
+    gameBoard.board[gameGrid.getAttribute("index")] = playersMarker;
+    console.log(gameBoard.board);
+}
+
 const gameBoard = (() => {
-    let index = 0;
     let board = Array(9)
 
     playersMarker = "X";
@@ -14,22 +28,32 @@ const gameBoard = (() => {
         noughts.style.backgroundColor = "yellow";
     })
     const gameBoardUI = document.querySelector('.game-board');
+    
     for (let i = 0; i < 9; i++) {
         let gameGrid = document.createElement('button');
         gameGrid.setAttribute("index", i)
         gameGrid.classList = "game-grid-object";
+        
         gameGrid.addEventListener('click', () => {
-            if (playersMarker == "X") {
-                noughts.style.visibility = 'hidden';
-            } else {
-                crosses.style.visibility = 'hidden';
-            }
-            gameGrid.innerText = playersMarker;
-            board[gameGrid.getAttribute("index")] = playersMarker;
+            setMove(playersMarker, board, noughts, crosses, gameGrid);
         })
         gameBoardUI.appendChild(gameGrid);
     }
+    gameRestart();
+    return {board};
 })();
+
+function gameRestart() {
+    const restart = document.getElementById("restartBtn");
+    const gameBoardItems = document.querySelector(".game-board").children;
+    const gameBoardItemsArray = [...gameBoardItems]; 
+    restart.addEventListener('click', () => {
+        gameBoardItemsArray.forEach((item) => item.textContent = '');
+        gameBoard.board = Array(9);
+    })
+};
+
+
 const Player = (() => {
     //functions
     
